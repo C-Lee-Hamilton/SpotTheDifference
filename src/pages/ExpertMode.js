3b999b508d80cad46a3ca5eb2c20970197c58f23
C-Lee-Hamilton <122707378+C-Lee-Hamilton@users.noreply.github.com>
2023-12-06 17:22:46 -0600

    more logic

--- src/pages/ExpertMode.js
+++ src/pages/ExpertMode.js
@@ -1,34 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "../components/grid";
 import samplePic1 from "../media/images/251.png";
 import samplePic2 from "../media/images/252.png";
-
+import Coordinates from "../components/coordinates";
 function ExpertMode() {
-  const [buttonStates, setButtonStates] = useState(Array(25).fill(false));
-  const [picture, setPicture] = useState(samplePic1);
+  const [starting, setStarting] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [buttonStates, setButtonStates] = useState(Array(100).fill(false));
+  const [clickedButtons, setClickedButtons] = useState([]);
+  const [winning, setWinning] = useState(false);
+  const [image, setImage] = useState(0);
+
+  const imageSrc = Coordinates[image].src;
 
   const handleButtonClick = (index) => {
     const newButtonStates = [...buttonStates];
     newButtonStates[index] = !newButtonStates[index];
     setButtonStates(newButtonStates);
+    setClickedButtons((prevClickedButtons) => {
+      if (newButtonStates[index]) {
+        return [...prevClickedButtons, index];
+      } else {
+        return prevClickedButtons.filter((btnIndex) => btnIndex !== index);
+      }
+    });
   };
+  const NextButton = () => {
+    setWinning(false);
+    setButtonStates(Array(100).fill(false));
+    setClickedButtons([]);
+    setImage(image + 1);
+    setTimeout(() => {
+      setImage(image + 1);
+    }, 5000);
+  };
+  const StartButton = () => {
+    setStarting(false);
+    setIsPlaying(true);
+    setTimeout(() => {
+      setImage(image + 1);
+    }, 5000);
+  };
+
+  useEffect(() => {
+    // Check for a specific winning combination (e.g., buttons 1, 2, and 3)
+    const winningCombination = Coordinates[image].solution;
+
+    if (
+      clickedButtons.length === winningCombination.length &&
+      clickedButtons.every((btnIndex) => winningCombination.includes(btnIndex))
+    ) {
+      console.log("You win!");
+
+      // Set the winning state to trigger the animation
+      setWinning(true);
+    }
+  }, [clickedButtons]);
 
   return (
     <div>
       <div className="pictureFrame">
-        <img className="expert-img" src={picture} alt="" />
+        {starting && <img className="expert-img-blur" src={imageSrc} alt="" />}
+        {isPlaying && <img className="expert-img" src={imageSrc} alt="" />}
 
         <div className="grid-container">
           <div className="expert-grid-box1">
             <Grid
               buttonStates={buttonStates}
               handleButtonClick={handleButtonClick}
+              winning={winning}
             />
           </div>
         </div>
       </div>
-
-      <button className="checkButton">Check</button>
+      {starting && (
+        <button className="checkButton" onClick={StartButton}>
+          Start
+        </button>
+      )}
+      {winning && (
+        <button className="checkButton" onClick={NextButton}>
+          Next
+        </button>
+      )}
     </div>
   );
 }
